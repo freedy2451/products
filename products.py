4a88c882b96dde05fd8890c9786802fe17b4a51d
Lucy <b237237@gmail.com>
2023-03-02 22:07:50 +0800

    v0.03

--- products.py
+++ products.py
@@ -10,6 +10,8 @@ for p in products:
     print("商品名稱為" + p[0] + ", 價格為" + p[1])
 
     
-with open('product.csv', 'w') as f:
-     for p in products:
+with open('product.csv', 'w', encoding = 'utf-8') as f:
+     f.write('商品,價格\n')
+     for p in products:        
         f.write(p[0] + ',' + p[1] + '\n')
+
